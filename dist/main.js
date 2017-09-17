e20dd64b301f8f907b728141aa8230e486977772
DeividasK <deividas.karzinauskas@gmail.com>
2017-09-17 19:17:21 +0100

    Removed console.log

--- dist/main.js
+++ dist/main.js
@@ -8,8 +8,7 @@ module.exports.loop = function () {
 
     const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     const upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    
-    console.log(harvesters);
+
     for (const creepName in harvesters) {
         spawn1.renewCreep(harvesters[creepName]);
     }
@@ -18,7 +17,7 @@ module.exports.loop = function () {
         var newName = spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
-    
+
     if(upgraders.length < 5 && upgraders.length < harvesters.length - 2 && spawn1.canCreateCreep([WORK,CARRY,MOVE]) === OK) {
         var newName = spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
