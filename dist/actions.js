92ef78b68b361c98c9a542d84eefbaf403aafce4
DeividasK <deividas.karzinauskas@gmail.com>
2017-09-17 21:22:37 +0100

    Fixed this reference.

--- dist/actions.js
+++ dist/actions.js
@@ -1,15 +1,17 @@
 const spawn1 = Game.spawns['Spawn1'];
 
-module.exports = {
-  moveToSpawn: (creep) => {
+const actions = {
+  moveToSpawn: function moveToSpawn(creep) {
     return creep.moveTo(spawn1);
   },
-  withdrawEnergy: (creep) => {
+  withdrawEnergy: function withdrawEnergy(creep) {
     const withdrawalStatus = creep.withdraw(spawn1, RESOURCE_ENERGY);
 
     switch(withdrawalStatus) {
       case ERR_NOT_IN_RANGE:
-        return this.moveToSpawn(creep);
+        return actions.moveToSpawn(creep);
     }
   }
 };
+
+module.exports = actions;
