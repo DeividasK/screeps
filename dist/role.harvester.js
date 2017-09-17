84fddf81604243203b1bb86760ca914dbe3fcff2
DeividasK <deividas.karzinauskas@gmail.com>
2017-09-17 20:57:55 +0100

    Changed to get only sources which have energy.

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -1,12 +1,11 @@
 function moveToEnergySource(creep) {
-    const sources = creep.room.find(FIND_SOURCES);
+    const sources = creep.room.find(FIND_SOURCES, {
+        filter: (source) => source.energy > 0
+    });
     if (sources.length === 0) return;
-    if (Game.creeps.length === 0) return;
 
-    const selectedSource = Game.creeps.length % sources.length;
-
-    if(creep.harvest(sources[selectedSource]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[selectedSource]);
+    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(sources[0]);
     }
 }
 
@@ -24,26 +23,25 @@ function moveToSpawn(creep) {
 }
 
 var roleHarvester = {
-
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        if (creep.ticksToLive < 300) {
-            return moveToSpawn(creep);
-        }
-
-      if(creep.carry.energy < creep.carryCapacity) {
-           return moveToEnergySource(creep);
-        }
-
-        const targetsWithCapacity = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => canStoreEnergy(structure) && hasCapacity(structure)
-        });
-
-        if(targetsWithCapacity.length > 0 && creep.transfer(targetsWithCapacity[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            return creep.moveTo(targetsWithCapacity[0]);
-        }
-
+  /** @param {Creep} creep **/
+  run: function(creep) {
+    if (creep.ticksToLive < 300) {
         return moveToSpawn(creep);
+    }
+
+    if(creep.carry.energy < creep.carryCapacity) {
+       return moveToEnergySource(creep);
+    }
+
+    const targetsWithCapacity = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => canStoreEnergy(structure) && hasCapacity(structure)
+    });
+
+    if(targetsWithCapacity.length > 0 && creep.transfer(targetsWithCapacity[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        return creep.moveTo(targetsWithCapacity[0]);
+    }
+
+    return moveToSpawn(creep);
   }
 };
 
