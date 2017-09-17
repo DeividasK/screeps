01abb7f108309d519a00de0510b32d441c9da2c3
DeividasK <deividas.karzinauskas@gmail.com>
2017-09-17 22:05:16 +0100

    Renew creeps only if the total count is less or equal to the count that needs to be maintained.

--- dist/mainControls.js
+++ dist/mainControls.js
@@ -1,10 +1,10 @@
 const spawn1 = Game.spawns['Spawn1'];
 
 const actions = {
-  maintain: function maintain(creepRole, count, creepBody) {
+  maintain: function maintain(creepRole, creepCount, creepBody) {
     const creepsByRole = _.filter(Game.creeps, (creep) => creep.memory.role === creepRole);
 
-    if(creepsByRole.length < count && spawn1.canCreateCreep(creepBody) === OK) {
+    if(creepsByRole.length < creepCount && spawn1.canCreateCreep(creepBody) === OK) {
         var newName = spawn1.createCreep(creepBody, undefined, {role: creepRole});
         console.log('Spawning new ' + creepRole + ': ' + newName);
     }
@@ -12,7 +12,9 @@ const actions = {
     _.find(Game.creeps, (creep) => creep.memory.role === creepRole);
 
     for (const creepName in creepsByRole) {
+      if (creepsByRole.length <= creepCount) {
         spawn1.renewCreep(creepsByRole[creepName]);
+      }
     }
   }
 };
