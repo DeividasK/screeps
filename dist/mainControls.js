2d1142901f2b7ec60bea79df5777a026c8f666e4
DeividasK <deividas.karzinauskas@gmail.com>
2017-09-18 07:12:38 +0100

    Check for spawning progress in the beginning.

--- dist/mainControls.js
+++ dist/mainControls.js
@@ -2,10 +2,14 @@ const spawn1 = Game.spawns['Spawn1'];
 
 const actions = {
   maintain: function maintain(creepRole, creepCount, creepBody) {
+    if (spawn1.spawning !== null) {
+      console.log('Spawning in progress: ' + spawn1.spawning);
+      return;
+    }
+    
     const creepsByRole = _.filter(Game.creeps, (creep) => creep.memory.role === creepRole);
 
-    if(creepsByRole.length < creepCount && spawn1.spawning === null && spawn1.canCreateCreep(creepBody) === OK) {
-        ;
+    if(creepsByRole.length < creepCount && spawn1.canCreateCreep(creepBody) === OK) {
         var newName = spawn1.createCreep(creepBody, undefined, {role: creepRole});
         console.log('Spawning new ' + creepRole + ': ' + newName);
     }
