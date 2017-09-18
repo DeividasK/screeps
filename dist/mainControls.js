b639f9c18edd705fc8e44d7b5e03da16a5f944a6
DeividasK <deividas.karzinauskas@gmail.com>
2017-09-18 07:35:49 +0100

    Blocking maintain statements.

--- dist/mainControls.js
+++ dist/mainControls.js
@@ -2,12 +2,18 @@ const spawn1 = Game.spawns['Spawn1'];
 
 const actions = {
   maintain: function maintain(creepRole, creepCount, creepBody) {
-    if (spawn1.spawning !== null) {
+    if (spawn1.spawning === null) {
+      console.log('Ready to spawn.');
+      spawn1.memory.spawning === false;
+    }
+
+    if (spawn1.memory.spawning === true) {
       console.log('Spawning in progress: ' + JSON.stringify(spawn1.spawning));
       return;
     }
 
-    console.log('Started spawning ' + creepRole + ' with ' + creepBody + ' body.');
+    console.log('Preparing to spawn ' + creepRole + ' with ' + creepBody + ' body.');
+    spawn1.memory.spawning = true;
 
     const creepsByRole = _.filter(Game.creeps, (creep) => creep.memory.role === creepRole);
 
