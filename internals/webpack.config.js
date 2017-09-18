bd6f7c10e33aa4947ff0d37545fa9e11681bea0a
DeividasK <deividas.karzinauskas@gmail.com>
2017-09-18 22:21:57 +0100

    Updated webpack config to target node.

--- internals/webpack.config.js
+++ internals/webpack.config.js
@@ -5,6 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
+  target: 'node',
   resolve: {
     modules: [
       path.resolve('src')
